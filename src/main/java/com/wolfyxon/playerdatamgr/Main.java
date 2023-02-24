d939ffbaf1a7dae68e4efa29c56f0dc877f5809a
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-24 16:12:55 +0100

    omg it finaly worgzgit add .111git add .

--- src/main/java/com/wolfyxon/playerdatamgr/Main.java
+++ src/main/java/com/wolfyxon/playerdatamgr/Main.java
@@ -1,12 +1,18 @@
 package com.wolfyxon.playerdatamgr;
 
 import com.wolfyxon.playerdatamgr.commands.TestCommand;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        getCommand("hi").setExecutor(new TestCommand());
+        PluginCommand hiCommand = getCommand("hi");
+        CommandExecutor hiClass = new TestCommand(this);
+        getLogger().info(hiCommand.toString());
+        getLogger().info(hiClass.toString());
+        hiCommand.setExecutor(hiClass);
         getLogger().info("PlayerDataManager has started");
     }
     @Override
