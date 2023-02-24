f900be9058339b1e41bfda4dc84c85c809986575
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-24 17:10:19 +0100

    Messages class

--- src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
+++ src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
@@ -4,9 +4,11 @@ import com.wolfyxon.playerdatamgr.commands.TemplateCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerDataMgr extends JavaPlugin {
+    Messages msgs = new Messages();
+
     @Override
     public void onEnable() {
-        getCommand("hi").setExecutor(new TemplateCommand(this));
+        getCommand("getuuid").setExecutor(new TemplateCommand(this));
         getLogger().info("PlayerDataManager has started");
     }
     @Override
