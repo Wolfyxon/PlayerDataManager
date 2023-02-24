03e758bddb9e4a03de9ee544c8ec8561a06f46aa
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-24 16:29:34 +0100

    refractor main class

--- src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
+++ src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
@@ -5,14 +5,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class PlayerDataMgr extends JavaPlugin {
     @Override
     public void onEnable() {
-        PluginCommand hiCommand = getCommand("hi");
-        CommandExecutor hiClass = new TestCommand(this);
-        getLogger().info(hiCommand.toString());
-        getLogger().info(hiClass.toString());
-        hiCommand.setExecutor(hiClass);
+        getCommand("hi").setExecutor(new TestCommand(this));
         getLogger().info("PlayerDataManager has started");
     }
     @Override
--- src/main/java/com/wolfyxon/playerdatamgr/commands/TestCommand.java
+++ src/main/java/com/wolfyxon/playerdatamgr/commands/TestCommand.java
@@ -1,20 +1,19 @@
 package com.wolfyxon.playerdatamgr.commands;
 
-import com.wolfyxon.playerdatamgr.Main;
+import com.wolfyxon.playerdatamgr.PlayerDataMgr;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class TestCommand implements CommandExecutor {
-    Main plugin;
-    public TestCommand(Main main){
+    PlayerDataMgr plugin;
+    public TestCommand(PlayerDataMgr main){
         plugin = main;
         main.getLogger().info("init");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        plugin.getLogger().info("EXEC");
         sender.sendMessage("wasup");
         return false;
     }
