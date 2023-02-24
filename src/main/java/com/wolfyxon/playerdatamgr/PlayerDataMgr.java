166ffbd8ac6d9dae542949eaf48d32297a680b8d
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-24 17:06:08 +0100

    Working on commands, added 'messages' class

--- src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
+++ src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
@@ -1,14 +1,12 @@
 package com.wolfyxon.playerdatamgr;
 
-import com.wolfyxon.playerdatamgr.commands.TestCommand;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.PluginCommand;
+import com.wolfyxon.playerdatamgr.commands.TemplateCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerDataMgr extends JavaPlugin {
     @Override
     public void onEnable() {
-        getCommand("hi").setExecutor(new TestCommand(this));
+        getCommand("hi").setExecutor(new TemplateCommand(this));
         getLogger().info("PlayerDataManager has started");
     }
     @Override
