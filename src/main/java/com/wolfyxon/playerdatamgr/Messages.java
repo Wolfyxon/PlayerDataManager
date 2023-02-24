4ec7b83edffc05bae94614a8c4d7145c1648574a
Wolfyxon <wolfyxon@gmail.com>
2023-02-24 17:10:19 +0100

    Messages class

--- src/main/java/com/wolfyxon/playerdatamgr/Messages.java
+++ src/main/java/com/wolfyxon/playerdatamgr/Messages.java
@@ -1,4 +1,11 @@
 package com.wolfyxon.playerdatamgr;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
 public class Messages {
+    public void errorMsg(CommandSender sender,String message){
+     sender.sendMessage(ChatColor.RED+message);
+    }
+
 }
