39bcf7fb6e46a1eaff4729051f1b7e2d587b1b49
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-25 13:59:01 +0100

    Removed unused JSONParser

--- src/main/java/com/wolfyxon/playerdatamgr/MojangAPI.java
+++ src/main/java/com/wolfyxon/playerdatamgr/MojangAPI.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.UUID;
 
-public class MojangAPI<JSONParser> {
+public class MojangAPI {
     PlayerDataMgr plugin;
     Utils utils = plugin.utils;
     public MojangAPI(PlayerDataMgr main){plugin = main;}
