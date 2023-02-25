f639d1ae9b3ec333881f36fccb7e37fe8ff88ee1
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-25 23:44:00 +0100

    Working on NBTManager

--- src/main/java/com/wolfyxon/playerdatamgr/NBTManager.java
+++ src/main/java/com/wolfyxon/playerdatamgr/NBTManager.java
@@ -1,18 +1,22 @@
 package com.wolfyxon.playerdatamgr;
 
-import net.querz.nbt.io.NBTUtil;
-import net.querz.nbt.io.NamedTag;
+import net.querz.nbt.io.*;
 import net.querz.nbt.tag.CompoundTag;
+import net.querz.nbt.tag.ListTag;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class NBTManager {
     PlayerDataMgr plugin;
     Utils utils;
     public NBTManager(PlayerDataMgr main){plugin=main;utils=plugin.utils;}
 
-    public CompoundTag parseFile(String path) {
+    public CompoundTag tagFromFile(String path) {
         if(!utils.file.isPathSafe(path)){return null;}
         File f = new File(path);
         try {
@@ -24,6 +28,22 @@ public class NBTManager {
             e.printStackTrace();
             return null;
         }
-
     }
+    public JSONObject tag2json(CompoundTag tag){
+        try {
+            String raw = SNBTUtil.toSNBT(tag).replace("I;",""); //possibly a library bug
+            return new JSONObject(raw);
+
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public JSONArray getPosition(CompoundTag tag) {
+        double result[] = new double[3];
+        JSONObject json = tag2json(tag);
+        return (JSONArray) json.get("Pos");
+    }
+
 }
