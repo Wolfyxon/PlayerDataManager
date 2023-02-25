f288aa55ea5e466089d2e235979ac07b090fc102
Wolfyxon <wolfyxon@gmail.com>
2023-02-25 18:25:00 +0100

    Testing NBT

--- src/main/java/com/wolfyxon/playerdatamgr/commands/GetDataCommand.java
+++ src/main/java/com/wolfyxon/playerdatamgr/commands/GetDataCommand.java
@@ -2,9 +2,14 @@ package com.wolfyxon.playerdatamgr.commands;
 
 import com.wolfyxon.playerdatamgr.PlayerDataMgr;
 import com.wolfyxon.playerdatamgr.Utils;
+import net.querz.nbt.io.*;
+import net.querz.nbt.tag.CompoundTag;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import java.io.File;
+import java.io.IOException;
 
 public class GetDataCommand implements CommandExecutor {
     PlayerDataMgr plugin;
@@ -13,9 +18,17 @@ public class GetDataCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         String world = "world";
+        File f = new File(utils.baseDir+"/world/playerdata/8faa349c-3def-4ac3-86a9-d6dbaf3b36af.dat");
+        try {
+            NamedTag tag = NBTUtil.read(f);
+            NamedTag tagFile = NBTUtil.read(f);
+            CompoundTag tagRoot = (CompoundTag) tag.getTag();
 
+            plugin.getLogger().info(String.valueOf(tagRoot.getFloat("Health")));
 
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
