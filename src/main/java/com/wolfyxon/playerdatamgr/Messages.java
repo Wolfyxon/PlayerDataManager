88f53caa531593134f51d523f8ae11065fe0af51
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-02-27 17:33:50 +0100

    Click teleport message for getpos

--- src/main/java/com/wolfyxon/playerdatamgr/Messages.java
+++ src/main/java/com/wolfyxon/playerdatamgr/Messages.java
@@ -1,5 +1,6 @@
 package com.wolfyxon.playerdatamgr;
 
+import com.wolfyxon.playerdatamgr.classes.Vector3;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
@@ -16,6 +17,9 @@ public class Messages {
         TextComponent msg = new TextComponent(plugin.utils.colored(message));
         msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,command));
         sender.spigot().sendMessage(msg);
+    }
+    public void clickTpMsg(CommandSender sender, String message, Vector3 pos){
+        clickCommandMsg(sender,message,"/teleport "+pos.toString());
     }
 
 }
