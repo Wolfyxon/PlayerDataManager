a0eb871381e3ade03cd4b3f3cf7509d79af73e3a
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-03-02 16:23:29 +0100

    Added the rest of commands to /playerdata help

--- src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
+++ src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
@@ -5,10 +5,15 @@ import com.wolfyxon.playerdatamgr.commands.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
 public final class PlayerDataMgr extends JavaPlugin {
     public Messages msgs = new Messages(this);
     public Utils utils = new Utils(this);
     public MojangAPI mojangAPI = new MojangAPI(this);
+    public Map<String, Map<String,Object>> commands;
 
     @Override
     public void onEnable() {
@@ -18,7 +23,7 @@ public final class PlayerDataMgr extends JavaPlugin {
         getCommand("seen").setExecutor(new SeenCommand(this));
         getCommand("firstjoined").setExecutor(new FirstJoinedCommand(this));
         getCommand("playerdata").setExecutor(new PlayerDataCommand(this));
-
+        commands = Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("PlayerDataManager")).getDescription().getCommands();
         Bukkit.getConsoleSender().sendMessage(utils.colored("&aPlayerDataManager has successfully loaded"));
 
 
