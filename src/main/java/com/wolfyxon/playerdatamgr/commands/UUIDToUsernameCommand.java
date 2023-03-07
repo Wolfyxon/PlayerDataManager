211bc1d426ba35b10f5661e6ec5ac69f1a5b427f
Wolfyxon <wolfyxon@gmail.com>
2023-03-07 17:52:05 +0100

    Initial UUIDToUsernameCommand

--- src/main/java/com/wolfyxon/playerdatamgr/commands/UUIDToUsernameCommand.java
+++ src/main/java/com/wolfyxon/playerdatamgr/commands/UUIDToUsernameCommand.java
@@ -2,17 +2,29 @@ package com.wolfyxon.playerdatamgr.commands;
 
 import com.wolfyxon.playerdatamgr.PlayerDataMgr;
 import com.wolfyxon.playerdatamgr.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class TemplateCommand implements CommandExecutor {
+public class UUIDToUsernameCommand implements CommandExecutor {
     PlayerDataMgr plugin;
     Utils utils;
-    public TemplateCommand(PlayerDataMgr main){plugin = main;utils = plugin.utils;}
+    public UUIDToUsernameCommand(PlayerDataMgr main){plugin = main;utils = plugin.utils;}
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        sender.sendMessage("wasup");
-        return false;
+        if(args.length == 0){
+            return true;
+        }
+        String input = args[0];
+        if(!utils.strIsUUID(input)){
+            return true;
+        }
+        OfflinePlayer plr = Bukkit.getOfflinePlayer(utils.str2uuid(input));
+        String username = plr.getName();
+        plugin.msgs.clickCopy(sender,"&1Username of &7"+input+"&r:\n"+username,username);
+
+        return true;
     }
 }
