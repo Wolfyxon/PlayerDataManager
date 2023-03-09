f79b160b9d95459a7f943d84f394e3e34d41fd83
Wolfyxon <wolfyxon@gmail.com>
2023-03-09 08:51:02 +0100

    Added relative time formatting method in Utils using PrettyTime lib

--- src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
+++ src/main/java/com/wolfyxon/playerdatamgr/PlayerDataMgr.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
